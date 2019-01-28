e6b57c07b9a9be9dd90310a0699fff1e30b77adb
Jessé Souza <jesse.rsouza@gmail.com>
2019-01-28 01:16:55 -0200

    Map, Filter e Reduce em Arrays

--- 04-lists/for-forin/2.map.js
+++ 04-lists/for-forin/2.map.js
@@ -17,7 +17,6 @@ async function main() {
         const result = await service.obterPessoas('a');
 
         //Primeira forma - Tradicional com a criação de uma variável auxiliar: 
-
         // const names   = [];
         // result.results.forEach(element => {
         //     names.push(element.name);
@@ -33,6 +32,7 @@ async function main() {
         //Terceira forma, a mais elegante e em uma linha:
         // const names = result.results.map(pessoa => pessoa.name);
 
+        
         //Quarta forma, criação de um map personalizado
         const names = result.results.meuMap((pessoa, indice) => `${indice} - ${pessoa.name}`)
 
