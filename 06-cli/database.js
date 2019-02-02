c930b0d9f893138ea70d304e1e35f120fa2e754e
Jessé Souza <jesse.rsouza@gmail.com>
2019-02-02 10:34:07 -0200

    Deletar itens

--- 06-cli/database.js
+++ 06-cli/database.js
@@ -1,12 +1,14 @@
 const {
-    readFile
+    readFile,
+    writeFile
 } = require('fs');
 
 const {
     promisify
 } = require('util');
 
-const readFileAsync = promisify(readFile);
+const readFileAsync  = promisify(readFile);
+const writeFileAsync = promisify(writeFile);
 
 //Outra forma de obter os dados dos arquivos
 // const dadosJson = require('./herois.json');
@@ -22,15 +24,53 @@ class Database {
         return JSON.parse(arquivo.toString()); //É necessário fazer o parse pq quando o arquivo vem, está um pouco diferente
     }
 
-    escreverArquivo() {
+    async escreverArquivo(dados) {
+        await writeFileAsync(this.NOME_ARQUIVO, JSON.stringify(dados));
 
+        return true;
+    }
+
+    async cadastrar(heroi) {
+        const dados = await this.obterDadosArquivo();
+        const id    = heroi.id <= 2 ? heroi.id : Date.now();
+
+        const heroiComID = {
+            id,
+            ...heroi
+        }
+
+        const dadosFinal = [
+            ...dados,
+            heroiComID
+        ]
+
+        const resultado = await this.escreverArquivo(dadosFinal);
+
+        return;
     }
 
     async listar(id) { //Sempre que tiver await, é necessário o async
         const dados          = await this.obterDadosArquivo();
-        const dadosFiltrados = dados.filter( (item) => (id ? (item.id === id): true));  //O id foi passado e é true? Se sim usa o item.id e procura quem tem o id específico. Senão manda true, ai pega a lista completa
+        const dadosFiltrados = dados.filter((item) => (id ? (item.id === id) : true));  //O id foi passado e é true? Se sim usa o item.id e procura quem tem o id específico. Senão manda true, ai pega a lista completa
         return dadosFiltrados;
-        }
     }
 
-    module.exports = new Database();
+    async deletar(id){
+        console.log(id);
+        if(!id){
+            return await this.escreverArquivo([])
+        }
+        
+        const dados  = await this.obterDadosArquivo();
+        const indice = dados.findIndex((item) => item.id === parseInt(id));
+
+        if(indice === -1){
+            throw Error('O usuário informado não existe');
+        }
+
+        dados.splice(indice, 1);
+        return await this.escreverArquivo(dados);
+    }
+}
+
+module.exports = new Database();
--- 06-cli/test.js
+++ 06-cli/test.js
@@ -10,7 +10,10 @@ const DEFAULT_ITEM_CADASTRAR = {
     id   : 1
 }
 describe('Suite de manipulação de Heróis', () => {
-
+    // before( async () => {
+    //     await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
+    // });
+    
     it('Deve pesquisar um herói, usando arquivos', async () => {
         const expected    = DEFAULT_ITEM_CADASTRAR;
         const [resultado] = await database.listar(expected.id);
@@ -19,9 +22,19 @@ describe('Suite de manipulação de Heróis', () => {
         deepEqual(resultado, expected);
     });
 
-    // it('Deve cadastrar um herói, usando arquivos', async () => {
-    //     const expected = DEFAULT_ITEM_CADASTRAR;
-    //     //
-    //     ok(null, expected);
-    // });
+    it('Deve cadastrar um herói, usando arquivos', async () => {
+        const expected  = DEFAULT_ITEM_CADASTRAR;
+        const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
+        const [actual]  = await database.listar(DEFAULT_ITEM_CADASTRAR.id);
+        
+        deepEqual(actual, expected);
+    });
+
+    it.only('Deve deletar um herói por id', async () => {
+        const expected  = true;
+        const resultado = await database.deletar(DEFAULT_ITEM_CADASTRAR.id);
+        //const [actual]  = await database.listar(DEFAULT_ITEM_CADASTRAR.id);
+        
+        deepEqual(resultado, expected);
+    });
 });
